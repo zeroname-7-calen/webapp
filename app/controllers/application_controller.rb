4fadb259192ba733acfafce16d084492d899cc2e
zeroname-7-calen <zero.7.calen@gmail.com>
2020-11-03 14:22:42 +0900

    usersテーブルにnameカラム追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,19 @@
 class ApplicationController < ActionController::Base
+
   include Banken
   protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  # 以下も追加
+  protected
+
+  def configure_permitted_parameters
+    added_attrs = [ :name, :email, :password, :password_confirmation ]
+    devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
+    devise_parameter_sanitizer.permit :account_update, keys: added_attrs
+    devise_parameter_sanitizer.permit :sign_in, keys: added_attrs
+  end
+  # 追加ここまで
 
   class LoginRequired < StandardError; end
   class Forbidden < StandardError; end
