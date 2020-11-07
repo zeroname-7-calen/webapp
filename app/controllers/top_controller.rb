bd809ca11c7a0cec6bf156da9e1c368550559505
zeroname-7-calen <zero.7.calen@gmail.com>
2020-11-07 16:01:53 +0900

    アンケート実装・再再修正

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -2,8 +2,6 @@ class TopController < ApplicationController
   def index
     @articles = Article.order(released_at: :desc).limit(5)
     # @fileuploads = @img_path
-    # authorize! @questionnaire
-    # authorize! @top
   end
 
   def about
