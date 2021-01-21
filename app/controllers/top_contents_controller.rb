02131bef33e66c917f334294777a414d87a3ceba
zeroname-7-calen <zero.7.calen@gmail.com>
2021-01-21 20:07:59 +0900

    日時指定を判定したい

--- app/controllers/top_contents_controller.rb
+++ app/controllers/top_contents_controller.rb
@@ -10,14 +10,16 @@ class TopContentsController < ApplicationController
 
   def create
     @top_content_start_date = top_content_params[:no_start_date]
-    if @top_content_start_date == "1"
-      # @top_content = TopContent.new(top_content_params)
-      @top_content = TopContent.new(specify_date_params)
+    @top_content_finish_date = top_content_params[:no_finish_date]
+    if @top_content_start_date == "1" && @top_content_finish_date =="1"
+      @top_content = TopContent.new(top_content_params)
+      @top_content.start_date = nil
+      @top_content.finish_date = nil
       @top_content.created_at = Time.now
         if @top_content.save
           redirect_to root_path, notice: "日付指定なしでトップ写真／動画をアップしました"
         end
-    elsif @top_content_start_date == "0"
+    elsif @top_content_start_date == "0" && @top_content_finish_date =="0"
       @top_content = TopContent.new(top_content_params)
       @top_content.created_at = Time.now
         if @top_content.save
@@ -29,8 +31,11 @@ class TopContentsController < ApplicationController
   def update
     @top_content = TopContent.find(params[:id])
     @top_content_start_date = top_content_params[:no_start_date]
-    if @top_content_start_date == "1"
+    @top_content_finish_date = top_content_params[:no_finish_date]
+    if @top_content_start_date == "1" && @top_content_finish_date == "1"
       @top_content.assign_attributes(top_content_params)
+      @top_content.start_date = nil
+      @top_content.finish_date = nil
       if @top_content.save
         redirect_to root_path, notice: "日時を指定せずにトップ写真／動画を更新しました"
       end
@@ -49,18 +54,6 @@ class TopContentsController < ApplicationController
   end
 
     private
-
-    def specify_date_params
-      params.require(:top_content).permit(
-        :title,
-        :url,
-        :caption,
-        # :start_date,
-        :no_start_date,
-        # :finish_date,
-        :no_finish_date
-      )
-    end
 
     def top_content_params
       params.require(:top_content).permit(
--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,13 +1,17 @@
 class TopController < ApplicationController
   def index
     @articles = Article.order(released_at: :desc).limit(5)
-    if @top_content_start_date == "0"
+    puts "あああああああ"
+    puts "いいいいいいい"
+    if @top_content = TopContent.where.not("start_date = nil")
+    # @top_content_start_date = top_content_params[:no_start_date]
+    # if @top_content_start_date == "0"
       @top_contents = TopContent.where("start_date <= ?", Time.now).where("finish_date >= ?", Time.now)
     else
+    # elsif TopContent.where("start_date = 1")
       @top_contents = TopContent.all
     end
-
-      # @top_contents = TopContent.all
+      @top_contents = TopContent.all
   end
 
   def about
