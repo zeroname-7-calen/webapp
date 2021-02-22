b558558352bd7a5893bcb47a206464e7af8c23ca
zeroname-7-calen <zero.7.calen@gmail.com>
2021-02-22 23:29:24 +0900

    記事検索機能改善

--- app/models/article.rb
+++ app/models/article.rb
@@ -7,15 +7,11 @@ class Article < ApplicationRecord
 
   has_rich_text :content
 
-  class << self
-    def search(query)
-      if query.present?
-        Article.where("title LIKE ?","%#{query}%")
-      else
-        @articles = Article.order(released_at: :desc)
-      end
-    end
-  end
+    scope :search, -> (search_param = nil) {
+    return if search_param.blank?
+      joins("INNER JOIN action_text_rich_texts ON action_text_rich_texts.record_id = articles.id AND action_text_rich_texts.record_type = 'Article'")
+      .where("action_text_rich_texts.body LIKE ? OR articles.title LIKE ? ", "%#{search_param}%", "%#{search_param}%")
+    }
 end
 
 
