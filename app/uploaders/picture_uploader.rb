ac7427c53034010dce22950395b2e56333858288
zeroname-7-calen <zero.7.calen@gmail.com>
2021-04-08 15:59:46 +0900

    uploader,credentia.yml変更

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -15,8 +15,29 @@ class PictureUploader < CarrierWave::Uploader::Base
       end
 
   # Choose what kind of storage to use for this uploader:
-  storage :file
+  # storage :file  コメントアウト（２０２１．０４．０８）
   # storage :fog
+
+# ======ここから追加=====
+  if Rails.env.production?
+    storage :fog
+  else
+    storage :file
+  end
+
+  if Rails.env.production?
+    CarrierWave.configure do |config|
+      config.fog_credentials = {
+        # Amazon S3用の設定
+        :provider              => 'AWS',
+        :region                => ENV['ap-northeast-1'],  # S3に設定したリージョン。
+        :aws_access_key_id     => ENV['S3_ACCESS_KEY'],
+        :aws_secret_access_key => ENV['S3_SECRET_KEY']
+      }
+      config.fog_directory     =  ENV['S3_BUCKET']
+    end
+
+# =====追加ここまで======
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
