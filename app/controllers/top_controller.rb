936c556e0f94739f435a7c027d3302940fc9eb7e
zeroname-7-calen <zero.7.calen@gmail.com>
2021-05-17 18:00:18 +0900

    cover_story時間指定ほか/再修正

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -2,7 +2,7 @@ class TopController < ApplicationController
   def index
     @articles = Article.where("start_at <= ?", Time.now).where("finish_at >= ?", Time.now).or(Article.where("start_at <= ?", Time.now).where(finish_at: nil)).or(Article.where(start_at: nil)).order(released_at: :desc).limit(5)
 
-    # @cover_stories = CoverStory.where("start_at <= ?", Time.now).where("finish_at >= ?", Time.now).or(CoverStory.where("start_at <= ?", Time.now).where(finish_at: nil)).or(CoverStory.where(start_at: nil)).order(created_at: :desc).limit(1)
+    @cover_stories = CoverStory.where("start_at <= ?", Time.now).where("finish_at >= ?", Time.now).or(CoverStory.where("start_at <= ?", Time.now).where(finish_at: nil)).or(CoverStory.where(start_at: nil)).order(created_at: :desc).limit(1)
 
     @top_contents = TopContent.where("start_at <= ?", Time.now).where("finish_at >= ?", Time.now).or(TopContent.where("start_at <= ?", Time.now).where(finish_at: nil)).or(TopContent.where(start_at: nil)).order(created_at: :desc).limit(1)
 
